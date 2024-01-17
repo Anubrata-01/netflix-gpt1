2544c897294d662e30b36c9c46ae513d8049033b
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-17 18:43:43 +0530

    small changes for small device

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,11 +4,11 @@ import About from './About'
 const Home = () => {
   return (
     <>  
-    <div className="w-full h-screen  relative ">
+    <div className="w-[420px] h-[800px] overflow-x-hidden overflow-y-hidden sm:w-full sm:h-screen  relative ">
         <Header/>
         <About/>
         <div className=''>
-          <img className=' absolute w-full h-full object-cover -top-1 ' src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="background img" />
+          <img className=' absolute w-full h-full sm:w-full sm:h-full object-cover -top-1 ' src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="background img" />
           <div className=" bg-black/70 fixed top-0 left-0 w-full h-full"/>
         </div>
     </div>
