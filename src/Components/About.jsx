2aec2494535d7c28563cf02671a3d814f270244a
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-21 16:43:14 +0530

    add responsivenes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="  relative top-60 sm:top-40">
         <div className='font-sans '>
-            <h1 className=' absolute left-8 sm:left-20 z-30  break-words text-4xl font-extrabold text-white sm:text-3xl'>The biggest Indian hits. The best Indian stories.
+            <h1 className=' absolute left-7 sm:left-20 z-30  break-words text-3xl font-extrabold text-white sm:text-3xl'>The biggest Indian hits. The best Indian stories.
              All streaming here.</h1>
 
         </div>
