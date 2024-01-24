0ebb12495a835932d12be70104accd8081525466
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-24 11:29:16 +0530

    optimize the code

--- src/Components/MovieContainer/SimilarMovieSection.jsx
+++ src/Components/MovieContainer/SimilarMovieSection.jsx
@@ -1,25 +1,16 @@
-import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import MovidCard from "./MovidCard";
 import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-// import useSimilarVideos from '../../CustomHooks/useSimilarVideos'
-import { addSimilarMovies } from "../../Redux Store/movieSlice";
-import { Api_options } from "../constant";
+import useFetchSimilarMovies from "../../CustomHooks/useFetchSimilarMovies";
+import { useMemo } from "react";
 
 const SimilarMovieSection = ({ similar }) => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const similar_Url =
-    "https://api.themoviedb.org/3/movie/" + similar + "/similar";
-  const fetchBysimilarvideo = async (url) => {
-    const movies = await fetch(url, Api_options);
-    const JsonMovies = await movies.json();
-    dispatch(addSimilarMovies(JsonMovies));
-  };
-  useEffect(() => {
-    fetchBysimilarvideo(similar_Url);
-  }, []);
+
+  const similar_Url = useMemo(() => {
+    return "https://api.themoviedb.org/3/movie/" + similar + "/similar";
+ }, [similar]);
+  useFetchSimilarMovies(similar_Url, dispatch);
+
   const Similar = useSelector((store) => store?.movie?.similarMovies);
   console.log(Similar);
   const Url = "https://image.tmdb.org/t/p/w500";
@@ -27,8 +18,7 @@ const SimilarMovieSection = ({ similar }) => {
   return (
     <div className="ml-52 mt-2 flex">
       {Similar?.results?.map((similar) => (
-        <NavLink key={similar?.id} to={"/movie/similar/"+similar?.id}>
-          {/* <MovidCard  moviedetails={similar}/> */}
+        <NavLink key={similar?.id} to={"/movie/similar/" + similar?.id}>
           <div>
             <div className="w-[175px] h-[114px] relative pl-2">
               <img
@@ -38,7 +28,6 @@ const SimilarMovieSection = ({ similar }) => {
               />
             </div>
           </div>
-          {/* navigate("/") */}
         </NavLink>
       ))}
     </div>
