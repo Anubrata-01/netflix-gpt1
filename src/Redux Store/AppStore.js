35041bbe3b0a0582d76194de9a917a5dfc54a3ad
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-28 19:09:29 +0530

    add gptslice

--- src/Redux Store/AppStore.js
+++ src/Redux Store/AppStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import appReducer from "./CreateSlice"
 import movieReducer from "./movieSlice"
+import gptReducer from "./gptSlice"
 
 export const store = configureStore({
   reducer: {
     app:appReducer,
-    movie:movieReducer
+    movie:movieReducer,
+    gptSlice:gptReducer,
   },
 })
