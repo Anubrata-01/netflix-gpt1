995f742302458909e0874ce6060293440ed3348c
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-28 19:04:34 +0530

    active GptSearch Functionality

--- src/Components/HeaderFile.jsx
+++ src/Components/HeaderFile.jsx
@@ -3,17 +3,25 @@ import Header from "./Header";
 import MainContainer from "./MovieContainer/MainContainer";
 import SecondaryContainer from "./MovieContainer/SecondaryContainer";
 import useAuthentic from "../CustomHooks/useAuthentic";
+import { useSelector } from "react-redux";
+import GptSearchPage from "./GPTSearch/GptSearchPage";
 
 const HeaderFile = ({
   path,
 }) => {
   useAuthentic(path);
+  const isShow=useSelector((store)=>store?.gptSlice?.gptShow);
+
 
   return (
     <div className="h-screen bg-black">
       <Header />
-      <MainContainer />
-      <SecondaryContainer/>
+        {
+          isShow?(<GptSearchPage/>):(<>
+           <MainContainer />
+        <SecondaryContainer />
+          </>)
+        }
     </div>
   );
 };
