ef1d49cbe140bc5cd05d498df7381151101248db
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-28 19:06:29 +0530

    gptMovie section

---
+++ src/Components/GPTSearch/GptMovieSuggestions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GptMovieSuggestions = () => {
+  return (
+    <div className='absolute top-[30%] z-30 text-white flex justify-center left-[40%] sm:top-[30%]'>
+      GptMovieSuggestions
+      </div>
+  )
+}
+
+export default GptMovieSuggestions;
