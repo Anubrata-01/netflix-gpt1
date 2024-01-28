a7e831620e84b47c3d70cc16b57c0c34244bf8fe
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-28 19:06:58 +0530

    sm changes

--- src/Components/MovieContainer/CastSection.jsx
+++ src/Components/MovieContainer/CastSection.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import Castcard from './Castcard'
 import useMoviecredits from '../../CustomHooks/useMoviecredits'
 
 const CastSection = ({userId}) => {
-  useMoviecredits()
-    const casts=useSelector((store)=>store?.movie?.movieCredits?.cast)
-    const filterdcast=casts?.filter((item)=>item.profile_path)
-    console.log(casts);
-    console.log(filterdcast)
-    const Url="https://image.tmdb.org/t/p/w500"
-    
-  return (
+ useMoviecredits(userId)
+ const casts=useSelector((store)=>store?.movie?.movieCredits?.cast)
+ console.log(casts)
+ const filterdcast = useMemo(() => casts?.filter((item)=>item.profile_path), [casts])
+ const Url="https://image.tmdb.org/t/p/w500"
+
+ return (
     <div className=' mt-1 overflow-x-scroll no-scrollbar'>
-    <div className=' ml-52 flex'>
+      <div className=' ml-52 flex'>
         {
           filterdcast?.map((actor)=>(
             <NavLink key={actor.id} to={"/movie/"+ actor.name}>
-              <Castcard Url={Url} path={actor.profile_path
-} actor={actor}/>
+              <Castcard Url={Url} path={actor.profile_path} actor={actor}/>
             </NavLink>
           ))
         }
+      </div>
     </div>
-    </div>
-  )
+ )
 }
+
 
 export default CastSection
