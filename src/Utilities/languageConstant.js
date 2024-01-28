214931537044dfaa8803926278406a5469cf9bef
Anubrata-01 <anubratachanda36@gmail.com>
2024-01-28 19:09:03 +0530

    stored support languages in a constant file

---
+++ src/Utilities/languageConstant.js
@@ -0,0 +1,21 @@
+const lang={
+    en:{
+        search:"Search",
+        placeHolder:"What would you like to search!"
+    },
+    hindi:{
+        search:"खोज",
+        placeHolder:"आप क्या खोजना चाहेंगे!"
+    },
+    bengali:{
+        search:"অনুসন্ধান করুন",
+        placeHolder:"আপনি কি খুঁজতে চান!"
+    },
+    french:{
+        search:"Recherche",
+        placeHolder:"Que voudriez-vous rechercher !"
+    }
+}
+export default lang;
+export const SupportLang=[{identifier:"en",name:"English"},{identifier:"hindi",name:"Hindi"},{identifier:"bengali",name:"Bengali"},{identifier:"french",name:"French"}]
+
