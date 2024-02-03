f66fffc8a1203e6a3db88bb83d1c4893c2ce5c8a
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-03 17:39:33 +0530

    updated

--- src/Components/constant.js
+++ src/Components/constant.js
@@ -5,3 +5,6 @@ export const Api_options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkY2YxODZkZTE0MDVkMDY5MzQ2ZTBjNzUxYmVjMDFhZiIsInN1YiI6IjY1NmRkODc2NGE0YmY2MDEwMzUxMjQ5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y2OyhFa5GEQIFQo7vQgEs0Iz0l3JT8xfTzDi7TubukQ'
     }
   };
+
+export const OpenAI_Key="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";
+export const Url="https://image.tmdb.org/t/p/w500";
