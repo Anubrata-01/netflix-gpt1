e3185ab495c9c991f0fc575cb407bc87d9a696bd
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-03 17:48:38 +0530

    no chnages

--- src/Components/MovieContainer/SecondaryContainer.jsx
+++ src/Components/MovieContainer/SecondaryContainer.jsx
@@ -4,7 +4,7 @@ import MovieList from "./MovieList";
 const SecondaryContainer = ({ movies, movies1, movies2 }) => {
   return (
     <div className="w-full bg-black relative">
-      <MovieList movies={movies} movies1={movies1} movies2={movies2} />
+      <MovieList  />
     </div>
   );
 };
