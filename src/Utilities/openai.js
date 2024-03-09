607b3d95c6f2a2b979d55cf90d4c1318a66d782e
Anubrata-01 <anubratachanda36@gmail.com>
2024-03-09 13:57:47 +0530

    configured gemini ai

--- src/Utilities/openai.js
+++ src/Utilities/openai.js
@@ -1,8 +1,60 @@
-import OpenAI from 'openai';
-import { OpenAI_Key } from '../Components/constant';
+// node --version # Should be >= 18
+// export default 
+import { useDispatch } from "react-redux";
+import { Gemini_Api_Key } from "../Components/constant";
+import { setResults } from "../Redux Store/gptSlice";
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
 
-const openai = new OpenAI({
-  apiKey: OpenAI_Key,
-  dangerouslyAllowBrowser: true // This is the default and can be omitted
-});
-export default openai;
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = Gemini_Api_Key;
+
+ const runChat=async(promt,dispatch)=>{
+  // const dispatch=useDispatch();
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const result = await chat.sendMessage(promt);
+  const response = result.response;
+  dispatch(setResults(response.text()))
+  console.log(response.text());
+}
+// runChat("what is react")
+
+  export default runChat;
